61dfcd5d97e6372bdc965dd056a16178447b45da
Yousef <86269600+MihsterRobot@users.noreply.github.com>
2024-03-09 19:01:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 URL = "https://realpython.github.io/fake-jobs/"
-page = requests.get(URL)  # Issue HTTP GET request to retrieve HTML data
-print(page.text)  # Print HTML data as text
+# Issue HTTP GET request to retrieve HTML data
+page = requests.get(URL) 
+# Print HTML data as text
+print(page.text) 
 
 # Using .content instead of .text avoids issues with character encoding
 # .content holds raw bytes, which can be decoded better than text representation
@@ -11,7 +14,7 @@ print(page.text)  # Print HTML data as text
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="ResultsContainer")  # Find specific HTML element by ID
 
-# .prettify() formats the parse tree such that each tag is on its own separate line with indentation
+# prettify() formats the parse tree such that each tag is on its own separate line with indentation
 print(results.prettify())
 
 # find_all() returns iterable containing all HTML for all job listings displayed on page
@@ -19,8 +22,8 @@ job_elements = results.find_all("div", class_="card-content")
 
 for job_element in job_elements:
     print(job_element.prettify(), end="\n"*2)
-
 print()
+
 for job_element in job_elements:
     title_element = job_element.find("h2", class_="title")
     company_element = job_element.find("h3", class_="company")
@@ -29,7 +32,7 @@ for job_element in job_elements:
     print(company_element.text.strip())
     print(location_element.text.strip(), "\n")
 
-# Passing anonymous function (lambda function) to string=, which looks at each
+# Passing anonymous function (lambda) to string=, which looks at each
 # <h2> element, converts it to lowercase, and checks for substring "python"
 python_jobs = results.find_all("h2", string=lambda text: "python" in text.lower())
 
